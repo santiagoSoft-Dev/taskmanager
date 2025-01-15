6d2b9d52fd91c086d6be44fee49d10963b508fd7
santiagoSoft-Dev <santiago.mahecha.per@gmail.com>
2025-01-15 15:36:36 -0500

    Update tasks manager

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_tasks, create_task, delete_task
+from .views import list_tasks, create_task, update_task, delete_task
 
 urlpatterns = [
     path('', list_tasks),
     path('new_task/', create_task, name='create_task'),
-    path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
+    path('update_task/<int:task_id>/', update_task, name='update_task'),
+    path('delete_task/<int:task_id>/', delete_task, name='delete_task')
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Task
 
-# Create your views here.
 def list_tasks(request):
     tasks = Task.objects.all()
     return render(request, 'list_tasks.html', {"tasks": tasks})
@@ -10,6 +9,17 @@ def create_task(request):
     task = Task(title=request.POST['title'], description=request.POST['description'])
     task.save()
     return redirect('/tasks/')
+
+def update_task(request, task_id):
+    task = Task.objects.get(id=task_id)  
+
+    if request.method == "POST": 
+        task.title = request.POST['title']
+        task.description = request.POST['description']
+        task.save()  
+        return redirect('/tasks/') 
+
+    return render(request, 'edit_task.html', {'task': task})
 
 def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
